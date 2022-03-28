9d999733ab6a75af95bb9d356d01761d80fbeacb
baudin999 <ckelkboom@protonmail.com>
2022-03-28 08:44:20 +0200

    styling stuff

--- components/Organisation.js
+++ components/Organisation.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Organisation({ organisation }) {
     if (organisation.html) {
         return (
-            <>
+            <div className="blog">
                 {organisation.html && <div dangerouslySetInnerHTML={{ __html: organisation.html }} />}
 
                 <h1>Authors</h1>
@@ -12,19 +12,19 @@ export default function Organisation({ organisation }) {
                         return <li key={author.id}><a href={`/${organisation.id}/${author.id}/`}>{author.name}</a></li>;
                     })}
                 </ul>
-            </>
-        );    
+            </div>
+        );
     }
     else {
         return (
-            <>
+            <div className="blog">
                 <h1>{organisation.name}</h1>
                 <ul>
                     {organisation.authors.map(author => {
                         return <li key={author.id}><a href={`/${organisation.id}/${author.id}/`}>{author.name}</a></li>;
                     })}
                 </ul>
-            </>
+            </div>
         );
     }
 }
