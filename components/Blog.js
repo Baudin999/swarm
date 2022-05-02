1eae376432fa28cf4ea6435d5342ca5b078049d8
baudin999 <ckelkboom@protonmail.com>
2022-05-02 15:37:04 +0200

    fixedd url error

--- components/Blog.js
+++ components/Blog.js
@@ -13,11 +13,11 @@ export default function Blog({ organisation, author, blog }) {
                 <div className="blog--title-bar">
                     <div className="blog--title-bar--author-pill"><Image src={author.image} alt="something" /></div>
                     <div className="blog--title-bar--organisation">
-                        <a className="accented hover" href={url(`/${organisation.id}`)}>{organisation.name}</a>
+                        <a className="accented hover" href={url(`/${organisation.id}/`)}>{organisation.name}</a>
                     </div>
                     <span className="sep">|</span>
                     <div className="blog--title-bar--author">
-                        <a className="accented hover" href={url(`/${organisation.id}/${author.id}`)}>{author.name}</a>
+                        <a className="accented hover" href={url(`/${organisation.id}/${author.id}/`)}>{author.name}</a>
                     </div>
                     <span className="sep">|</span>
                     <div className="blog--title-bar--read-time">
